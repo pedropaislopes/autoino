39492c89b04dfe6acea466db46b90419c8b91eb4
Pedro Pais Lopes <pedropaislopes@gmail.com>
2018-11-16 18:48:29 -0200

    Correção na pinagem e encoders dos dimmers da sala principal.

--- autoino-serial/dadosCircdimmers.h
+++ autoino-serial/dadosCircdimmers.h
@@ -7,13 +7,13 @@
 */
 
 Dimmer dimmers[] = {
-  Dimmer(7, DIMMER_RAMP), // SCP
-  Dimmer(53, DIMMER_RAMP) // VRD
+  Dimmer(53, DIMMER_RAMP), // VRD
+  Dimmer(7, DIMMER_RAMP) // SCP
 };
 
 Encoder encoders[] = {
-  Encoder(19, 42), // SCP
-  Encoder(18, 43)  // VRD
+  Encoder(18, 20), // VRD
+  Encoder(19, 42)  // SCP
 };
 
 const int nCircdimmers = 2;
@@ -23,22 +23,16 @@ Circdimmer circdimmers[nCircdimmers];
 // Define dados dos Circdimmers
 void defineCircdimmers(void)
 {
-  circdimmers[0].pinoRele = 52;
-  circdimmers[0].encSW = 40;
+  circdimmers[0].pinoRele = 51;
+  circdimmers[0].encSW = 41;
   circdimmers[0].counter = 0;
   circdimmers[0].oldPos = 0;
-  circdimmers[0].nome = "CD.SCP";
-<<<<<<< HEAD
-  circdimmers[1].pinoRele = 51;
-  circdimmers[0].encSW = 41;
-=======
-  circdimmers[1].pinoSwitch = 4;
-  circdimmers[1].outputA = 12;
-  circdimmers[1].outputB = 11;
->>>>>>> 96ade752c4f9e1ba3e11545f00eca863375126e2
+  circdimmers[0].nome = "D1.VRD";
+  circdimmers[1].pinoRele = 52;
+  circdimmers[1].encSW = 40;
   circdimmers[1].counter = 0;
   circdimmers[1].oldPos = 0;
-  circdimmers[1].nome = "CD.VRD";
+  circdimmers[1].nome = "D0.SCP";
 }
 
 void setupCircdimmers()
@@ -106,7 +100,11 @@ void encoderCircdimmers()
 
     if (pos != circdimmers[i].oldPos) {
 
-      circdimmers[i].counter = circdimmers[i].counter + (pos - circdimmers[i].oldPos);
+/*      Serial.print(i);
+      Serial.print(" ");
+      Serial.println(pos); */
+
+      circdimmers[i].counter = circdimmers[i].counter + (pos - circdimmers[i].oldPos) / 2;
 
       if (circdimmers[i].counter > 100)
         circdimmers[i].counter = 100;
@@ -116,32 +114,11 @@ void encoderCircdimmers()
       if (circdimmers[i].counter > 0) {
 
         ligaCircdimmer(&circdimmers[i]);
-<<<<<<< HEAD
 
       } else {
 
         desligaCircdimmer(&circdimmers[i]);
       }
-=======
-        circdimmers[i].counter = circdimmers[i].counter + 10;
-        if (circdimmers[i].counter > 100)
-          circdimmers[i].counter = 100;
-
-      } else {
-
-        circdimmers[i].counter = circdimmers[i].counter - 10;
-        if (circdimmers[i].counter <= 0) {
-          desligaCircdimmer(&circdimmers[i]);
-          circdimmers[i].counter = 0;
-        }
-
-      }
-//     Serial.print(i);
-//     Serial.print("Position: ");
-//     Serial.println(circdimmers[i].counter);
-    }
-    circdimmers[i].aLastState = circdimmers[i].aState; // Updates the previous state of the outputA with the current state
->>>>>>> 96ade752c4f9e1ba3e11545f00eca863375126e2
 
       dimmers[i].set(circdimmers[i].counter);
 
